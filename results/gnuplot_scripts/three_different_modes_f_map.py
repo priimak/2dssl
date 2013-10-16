f13fb03305d790932c0e9439b7af522624e62f77
Dmitri Priimak <priimak@gmail.com>
2013-10-15 21:00:11 -0700

    now takes bz2 files

--- results/gnuplot_scripts/three_different_modes_f_map.py
+++ results/gnuplot_scripts/three_different_modes_f_map.py
@@ -10,7 +10,7 @@ fig  = plt.figure(figsize=(18, 6))
 fig.subplots_adjust(left=0.0, right=1, top=1, bottom=0, wspace=0.08, hspace=0.09)
 
 ax = fig.add_subplot(131)
-data = np.genfromtxt('B=4/E_dc=6_mu=116_alpha=0.9496_B=4.0_frame.data',
+data = np.genfromtxt('B=4/E_dc=6_mu=116_alpha=0.9496_B=4.0_frame.data.bz2',
                      delimiter=' ', 
                      names=['phi_x', 'phi_y', 'f'])
 
@@ -30,7 +30,7 @@ ax.set_ylim(-4.5,0.6)
 ax.text(-2.6, 0, '(a)', color='white', fontsize=28)
 
 ax = fig.add_subplot(132)
-data = np.genfromtxt('B=4/E_dc=8_mu=116_alpha=0.9496_B=4.0_frame.data',
+data = np.genfromtxt('B=4/E_dc=8_mu=116_alpha=0.9496_B=4.0_frame.data.bz2',
                      delimiter=' ', 
                      names=['phi_x', 'phi_y', 'f'])
 
@@ -50,7 +50,7 @@ ax.set_ylim(-4.5,0.6)
 ax.text(-2.6, 0, '(b)', color='white', fontsize=28)
 
 ax = fig.add_subplot(133)
-data = np.genfromtxt('B=4/E_dc=10_mu=116_alpha=0.9496_B=4.0_frame.data',
+data = np.genfromtxt('B=4/E_dc=10_mu=116_alpha=0.9496_B=4.0_frame.data.bz2',
                      delimiter=' ', 
                      names=['phi_x', 'phi_y', 'f'])
 
